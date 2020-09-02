710c3e137b2ff4948d9ac1a548afbfba258b479e
LordMero <graz@x1.localdomain>
2020-09-02 22:19:25 +0100

    added patch for clickable bar - doesn't really work though

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+    {"", "internet", 60, 9},
+    {"", "pacupdate", 60, 9},
+    {"", "volume2", 0, 10},
+    {"", "battery2", 1, 11},
+	{"", "date '+%a, %b %d  %I:%M%p'",					60,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
